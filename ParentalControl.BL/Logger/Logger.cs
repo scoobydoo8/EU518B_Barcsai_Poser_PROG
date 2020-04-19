a3471b09139941a61dd378de08aed02c7370d674
Dominik Barcsai <barcsai.dominik@gmail.com>
2020-04-19 20:35:34 +0200

    Move Logger class.

--- ParentalControl.BL/Logger/Logger.cs
+++ ParentalControl.BL/Logger/Logger.cs
@@ -2,7 +2,7 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-namespace ParentalControl
+namespace ParentalControl.BL
 {
     using System;
     using System.Collections.Generic;
