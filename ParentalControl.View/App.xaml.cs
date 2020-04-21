e6c9c0956baa33433783822a94f0c7064cc19282
Dominik Barcsai <barcsai.dominik@gmail.com>
2020-04-21 14:07:56 +0200

    Move Logger class.

--- ParentalControl.View/App.xaml.cs
+++ ParentalControl.View/App.xaml.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace ParentalControl.View
 {
@@ -14,5 +15,12 @@ namespace ParentalControl.View
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var exceptionMessage = e.Exception.GetType().Name + ":" + e.Exception.Message;
+            Logger.Get().LogException(exceptionMessage);
+            MessageBox.Show("Hiba történt:\n" + e.Exception.Message, "Hina!", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
     }
 }
